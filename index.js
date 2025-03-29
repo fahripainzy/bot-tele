ef3daf3cb9719f464888a64020732da97f18e8b9
fahripainzy <rayyhansem@gmail.com>
2025-03-30 04:15:13 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"; // Pastikan import dengan benar
+const app = express();
+
+const PORT = process.env.PORT || 3000; // Gunakan port dari Render
+
+app.get("/", (req, res) => {
+  res.send("Server berjalan dengan sukses di Render!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server berjalan di port ${PORT}`);
+});
+
+start("main.js");
